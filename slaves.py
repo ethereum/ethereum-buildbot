34682028b5a095e78599756ee19ff3ecad01fba1
caktux <caktux@gmail.com>
2015-03-31 12:15:20 -0400

    increase spot price

--- slaves.py
+++ slaves.py
@@ -3,7 +3,7 @@
 # @Author: caktux
 # @Date:   2015-02-23 13:42:42
 # @Last Modified by:   caktux
-# @Last Modified time: 2015-03-31 12:05:15
+# @Last Modified time: 2015-03-31 12:14:14
 
 ####### BUILDSLAVES
 
@@ -29,7 +29,7 @@ for slave in json.load(open("slaves.json")):
             region='us-east-1',
             placement='b',
             spot_instance=True,
-            max_spot_price=0.01,
-            price_multiplier=1.05))
+            max_spot_price=0.02,
+            price_multiplier=1.15))
     else:
         slaves.append(BuildSlave(slave['name'], slave['password']))
