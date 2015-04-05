3f3a6c98311a81835ee92ac14d9eb69f0ba9a3f6
caktux <caktux@gmail.com>
2015-04-05 05:52:16 -0400

    let pyethapp run step flunkOnFailure

--- factories/pyethapp.py
+++ factories/pyethapp.py
@@ -3,7 +3,7 @@
 # @Author: caktux
 # @Date:   2015-02-23 14:50:15
 # @Last Modified by:   caktux
-# @Last Modified time: 2015-04-05 05:11:52
+# @Last Modified time: 2015-04-05 05:46:11
 
 import factory
 reload(factory)
@@ -13,12 +13,12 @@ def pyethapp_factory(branch='master'):
     factory = BuildFactory()
     for step in [
         Git(
-            haltOnFailure = True,
-            logEnviron = False,
+            haltOnFailure=True,
+            logEnviron=False,
             repourl='https://github.com/ethereum/pyethapp.git',
             branch=branch,
             mode='full',
-            method = 'copy',
+            method='copy',
             codebase='pyethapp',
             retry=(5, 3)
         ),
@@ -30,31 +30,30 @@ def pyethapp_factory(branch='master'):
             property="version"
         ),
         ShellCommand(
-            haltOnFailure = True,
-            logEnviron = False,
+            haltOnFailure=True,
+            logEnviron=False,
             name="pip-requirements",
             description="installing requirements",
             descriptionDone="install requirements",
             command=["pip", "install", "-r", "requirements.txt"]
         ),
         ShellCommand(
-            haltOnFailure = True,
-            logEnviron = False,
+            haltOnFailure=True,
+            logEnviron=False,
             name="upgrade-requirements",
             description="upgrading test requirements",
             descriptionDone="upgrade test requirements",
             command=["pip", "install", "--upgrade", "--no-deps", "-r", "requirements.txt"]
         ),
         ShellCommand(
-            haltOnFailure = True,
-            logEnviron = False,
+            haltOnFailure=True,
+            logEnviron=False,
             name="pip-install",
             description="installing",
             descriptionDone="install",
             command=["pip", "install", "-e", "."]
         ),
         ShellCommand(
-            flunkOnFailure=False,
             logEnviron=False,
             description="running",
             descriptionDone="run",
