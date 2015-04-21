750ed596d1bcb76d8f9f983d679d191bbb064c7c
caktux <caktux@gmail.com>
2015-04-21 08:21:41 -0400

    windozzz...

--- factories/go_ethereum_windows.py
+++ factories/go_ethereum_windows.py
@@ -3,7 +3,7 @@
 # @Author: caktux
 # @Date:   2015-04-20 22:03:29
 # @Last Modified by:   caktux
-# @Last Modified time: 2015-04-21 08:15:18
+# @Last Modified time: 2015-04-21 08:21:19
 
 import factory
 reload(factory)
@@ -16,7 +16,7 @@ from go_ethereum import get_short_revision_go
 def _go_cmds_win(branch='master'):
     cmds = [
         "mkdir %GOPATH%\\src\\github.com\\ethereum",
-        "xcopy /C/T/E/H/Y * %GOPATH%\\src\\github.com\\ethereum\\go-ethereum\\"
+        "xcopy /C/T/E/H/Y *.* %GOPATH%\\src\\github.com\\ethereum\\go-ethereum\\"
     ]
 
     return " && ".join(cmds)
