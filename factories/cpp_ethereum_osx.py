3eb1a0dfa50569fd2884d59b16049d3ecac249dd
caktux <caktux@gmail.com>
2015-05-10 05:56:24 -0400

    add -DETHASHCL=1 to main builds

--- factories/cpp_ethereum_osx.py
+++ factories/cpp_ethereum_osx.py
@@ -14,6 +14,7 @@ reload(cpp_ethereum)
 from cpp_ethereum import *
 
 def cmake_osx_cmd(cmd=[], ccache=True, evmjit=False, headless=True):
+    cmd.append("-DETHASHCL=1")
     cmd.append("-DFATDB=1")
     cmd.append("-DBUNDLE=default")
     if headless:
