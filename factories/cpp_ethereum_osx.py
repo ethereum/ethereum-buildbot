b5dd2894120ceee04ca43fd6f408e2404ccd12ad
subtly <subtly@users.noreply.github.com>
2015-07-08 10:00:42 -0700

    Include ethash w/macos build

--- factories/cpp_ethereum_osx.py
+++ factories/cpp_ethereum_osx.py
@@ -16,6 +16,7 @@ from cpp_ethereum import *
 def cmake_osx_cmd(cmd=[], ccache=True, evmjit=False, headless=True):
     cmd.append("-DFATDB=1")
     cmd.append("-DBUNDLE=default")
+    cmd.append("-DETHASHCL=1")
     if headless:
         cmd.append("-DGUI=0")
     if evmjit:
